444cfcf4dd34f36323d53d95a115b67682a11996
Łukasz Brzózko <lukasz9099@gmail.com>
2019-05-01 18:39:37 +0200

    Add header responsiveness (landscape)
    - Add more breakpoints for veritcal responsiveness
    - Add "_header_topbar.scss" file
    - Change positioning parameters of ".text-container" in header.scss
    - Move the ".top-bar" properties to "_header_topbar" module
    - Change the line-hight for the title and the subtitle text
    - Add mixin for landscape media query in "_mixins.scss"
    - Add comment titles for all modules
    - Add the function that shrinks the top bar in script.js
    - Add comments in script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const anchors = document.querySelectorAll(".scroll");
   const btn = document.querySelector(".btn-menu");
+  const topBar = document.querySelector(".top-bar");
 
+  // Setting a smooth scroll for all the anchors from the top bar
   const setSmoothScroll = function(e) {
     e.preventDefault();
     const hash = this.hash; //href
@@ -15,8 +17,14 @@ document.addEventListener("DOMContentLoaded", () => {
   anchors.forEach(anchor => {
     anchor.addEventListener("click", setSmoothScroll);
   });
+
+  // Menu hamburger's click handler
   btn.addEventListener("click", () => {
-    btn.classList.toggle("active");
-    document.querySelector(".top-bar").classList.toggle("expand");
+    topBar.classList.toggle("expand");
+  });
+
+  // Shrink the top bar when the window is resized manually
+  window.addEventListener("resize", () => {
+    if (innerWidth > 900) topBar.classList.remove("expand");
   });
 });
